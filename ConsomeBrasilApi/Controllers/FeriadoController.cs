5e13f18a7a332d2a0cfa08dfd332668067ef6403
Rafael Kowalski <rafaelmk06@gmail.com>
2023-02-13 13:27:17 -0300

    Criado o controller

---
+++ ConsomeBrasilApi/Controllers/FeriadoController.cs
@@ -0,0 +1,22 @@
+﻿using ConsomeBrasilApi.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsomeBrasilApi.Controllers
+{
+    public class FeriadoController : Controller
+    {
+        public readonly IFeriado _feriadoService;
+
+        [HttpGet("Busca/Feriados")]
+        public async Task<IActionResult> BuscaTodosFeriadosPorAno(string ano)
+        {
+            var response = await _feriadoService.BuscaTodosFeriadosPorAno(ano);
+
+            if (response.CodigoHttp == System.Net.HttpStatusCode.OK)
+                return Ok(response.DadosRetorno);
+
+            else
+                return StatusCode((int)response.CodigoHttp, response.ErroRetorno);
+        }
+    }
+}
