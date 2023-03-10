247485cd53c28d92a3936760cd578abeb871c6e3
Rafael Kowalski <rafaelmk06@gmail.com>
2023-03-10 15:16:34 -0300

    Criando controller Saudacao

---
+++ ConsomeBrasilApi/Controllers/SaudacaoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConsomeBrasilApi.Controllers
+{
+    [Route("[Controller]")]
+    [ApiController]
+    public class SaudacaoController : Controller
+    {
+        [HttpGet]
+        public IActionResult Saudacao(string nome)
+        {
+            var mensagem = $"Ola {nome}, seja bem vindo ao projeto ConsomeBrasilApi \n" + DateTime.Now.ToString();
+
+            return Ok(mensagem);
+        }
+    }
+}
